deba08d101718ca27b4bac4bcb1eba951382d6e9
Constantin Nickel <constantin.nickel@gmail.com>
2019-02-26 15:55:12 +0100

    create embeds for mod events

--- src/commands/subs.rs
+++ src/commands/subs.rs
@@ -1,17 +1,19 @@
 use std::time::Duration;
 
+use either::{Left, Right};
 use futures::future::{self, Either};
 use futures::{Future, Stream};
 use log::{debug, warn};
 use modio::filter::{Operator, Order};
 use modio::games::GamesListOptions;
-use modio::mods::ModsListOptions;
+use modio::mods::{Event, EventType, Mod, ModsListOptions};
 use modio::EventListOptions;
 use modio::Modio;
 use serenity::prelude::*;
 use tokio::runtime::TaskExecutor;
 use tokio::timer::Interval;
 
+use crate::commands::prelude::*;
 use crate::db::Subscriptions;
 use crate::util;
 
@@ -93,6 +95,52 @@ command!(
     }
 );
 
+struct Notification<'a> {
+    event: &'a Event,
+    mod_: &'a Mod,
+}
+
+impl<'a> Notification<'a> {
+    fn new(n: (&'a Event, &'a Mod)) -> Notification<'a> {
+        Notification {
+            event: n.0,
+            mod_: n.1,
+        }
+    }
+
+    fn is_ignored(&self) -> bool {
+        use EventType::*;
+        match self.event.event_type {
+            UserTeamJoin | UserTeamLeave | UserSubscribe | UserUnsubscribe | ModTeamChanged => true,
+            _ => false,
+        }
+    }
+
+    fn create_message(&self, m: CreateMessage) -> CreateMessage {
+        let desc = match self.event.event_type {
+            EventType::ModEdited => Left("The mod has been edited."),
+            EventType::ModAvailable => Left("A new mod is available."),
+            EventType::ModUnavailable => Left("The mod is now unavailable."),
+            EventType::ModfileChanged => Right(format!(
+                "The primary file changed. [link]({})",
+                self.mod_
+                    .modfile
+                    .as_ref()
+                    .map(|f| f.download.binary_url.to_string())
+                    .unwrap_or_default(),
+            )),
+            EventType::ModDeleted => Left("The mod has been permanently deleted."),
+            _ => Left("ignored event"),
+        };
+        m.embed(|e| {
+            e.title(&self.mod_.name)
+                .url(&self.mod_.profile_url)
+                .description(desc)
+                .thumbnail(&self.mod_.logo.thumb_640x360)
+        })
+    }
+}
+
 pub fn task(
     client: &Client,
     modio: Modio,
@@ -149,9 +197,14 @@ pub fn task(
                                     .position(|e| e.mod_id == a.id)
                                     .cmp(&events.iter().position(|e| e.mod_id == b.id))
                             });
-                            for (e, m) in events.iter().zip(mods.iter()) {
+                            let it = events
+                                .iter()
+                                .zip(mods.iter())
+                                .map(Notification::new)
+                                .filter(|n| !n.is_ignored());
+                            for n in it {
                                 for (channel, _) in &channels {
-                                    let _ = channel.say(format!("{} {}", m.name, e.event_type));
+                                    let _ = channel.send_message(|m| n.create_message(m));
                                 }
                             }
                             Ok(())
