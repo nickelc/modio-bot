2ce68d2785b5e23f14e7df2790b09c8437dbe04c
Constantin Nickel <constantin.nickel@gmail.com>
2019-05-14 01:50:54 +0200

    workaround for serenity-rs/serenity#555

--- src/main.rs
+++ src/main.rs
@@ -119,8 +119,31 @@ fn try_main() -> CliResult {
                 }
                 _ => {}
             })
-            .help(help_commands::with_embeds),
+            .help(workaround::help),
     );
     client.start()?;
     Ok(())
+}
+
+mod workaround {
+    use std::collections::HashMap;
+    use std::sync::Arc;
+
+    use super::help_commands;
+    use serenity::client::Context;
+    use serenity::framework::standard::{Args, CommandError, CommandGroup, HelpOptions};
+    use serenity::model::channel::Message;
+
+    pub fn help(
+        ctx: &mut Context,
+        msg: &Message,
+        options: &HelpOptions,
+        commands: HashMap<String, Arc<CommandGroup>>,
+        args: &Args,
+    ) -> Result<(), CommandError> {
+        if msg.author.bot {
+            return Ok(());
+        }
+        help_commands::with_embeds(ctx, msg, options, commands, args)
+    }
 }
