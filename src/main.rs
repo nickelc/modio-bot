0e00595b6d8eeb305b8899a1685bca5ad80509fb
Constantin Nickel <constantin.nickel@gmail.com>
2019-06-24 04:33:06 +0200

    remove the main doc comment

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-//! ![MODBOT logo][logo]
-//!
-//! ![Rust version][rust-version]
-//! ![Rust edition][rust-edition]
-//! ![License][license-badge]
-//!
-//! MODBOT is a Discord bot for [mod.io] using [`modio-rs`] and [`serenity`].
-//!
-//!
-//! [rust-version]: https://img.shields.io/badge/rust-1.31%2B-blue.svg
-//! [rust-edition]: https://img.shields.io/badge/edition-2018-red.svg
-//! [license-badge]: https://img.shields.io/badge/license-MIT%2FApache--2.0-blue.svg
-//! [logo]: https://raw.githubusercontent.com/nickelc/modio-bot/master/logo.png
-//! [mod.io]: https://mod.io
-//! [`modio-rs`]: https://github.com/nickelc/modio-rs
-//! [`serenity`]: https://github.com/serenity-rs/serenity
 #![deny(rust_2018_idioms)]
 
 #[macro_use]
