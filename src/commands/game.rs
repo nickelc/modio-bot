c8be7655782ad3c5bd35147d0e914ca5384678b7
Constantin Nickel <constantin.nickel@gmail.com>
2022-11-27 18:10:39 +0100

    Show game details if only one game is found
    This also fixes the problem of not responding when no games are found.

--- src/commands/game.rs
+++ src/commands/game.rs
@@ -1,17 +1,22 @@
 use std::fmt::Write;
 
-use futures_util::TryStreamExt;
+use futures_util::{Stream, TryStreamExt};
 use modio::filter::prelude::*;
+use modio::games::Game;
 use twilight_model::application::command::{Command, CommandType};
 use twilight_model::application::interaction::application_command::{
     CommandData, CommandDataOption, CommandOptionValue,
 };
 use twilight_model::application::interaction::Interaction;
 use twilight_model::channel::message::embed::EmbedField;
+use twilight_model::channel::message::Embed;
 use twilight_util::builder::command::{CommandBuilder, StringBuilder};
 use twilight_util::builder::embed::{EmbedBuilder, ImageSource};
 
-use super::{create_response, defer_response, update_response_from_content, EphemeralMessage};
+use super::{
+    create_response, defer_response, update_response_content, update_response_from_content,
+    EphemeralMessage,
+};
 use crate::bot::Context;
 use crate::error::Error;
 use crate::util::ContentBuilder;
@@ -49,19 +54,37 @@ pub async fn games(
 
     defer_response(ctx, interaction).await?;
 
-    let games = ctx
-        .modio
-        .games()
-        .search(filter)
-        .iter()
-        .await?
-        .try_fold(ContentBuilder::new(4000), |mut buf, game| {
-            let _ = writeln!(&mut buf, "{}. {}", game.id, game.name);
-            async { Ok(buf) }
-        })
-        .await?;
+    let mut games = ctx.modio.games().search(filter).iter().await?;
 
-    update_response_from_content(ctx, interaction, "Games", &games.content).await
+    match games.size_hint() {
+        (0, _) => update_response_content(ctx, interaction, "No games found.").await,
+        (1, _) => {
+            let game = games.try_next().await?;
+            if let Some(game) = game {
+                let embed = create_embed(game);
+
+                ctx.interaction()
+                    .update_response(&interaction.token)
+                    .content(Some("Found 1 game."))?
+                    .embeds(Some(&[embed]))?
+                    .await?;
+
+                Ok(())
+            } else {
+                update_response_content(ctx, interaction, "No games found.").await
+            }
+        }
+        _ => {
+            let games = games
+                .try_fold(ContentBuilder::new(4000), |mut buf, game| {
+                    let _ = writeln!(&mut buf, "{}. {}", game.id, game.name);
+                    async { Ok(buf) }
+                })
+                .await?;
+
+            update_response_from_content(ctx, interaction, "Games", &games.content).await
+        }
+    }
 }
 
 pub async fn game(ctx: &Context, interaction: &Interaction) -> Result<(), Error> {
@@ -80,6 +103,17 @@ pub async fn game(ctx: &Context, interaction: &Interaction) -> Result<(), Error>
 
     let game = ctx.modio.game(game_id).get().await?;
 
+    let embed = create_embed(game);
+
+    ctx.interaction()
+        .update_response(&interaction.token)
+        .embeds(Some(&[embed]))?
+        .await?;
+
+    Ok(())
+}
+
+fn create_embed(game: Game) -> Embed {
     let mut embed = EmbedBuilder::new()
         .title(game.name)
         .url(game.profile_url.to_string())
@@ -108,11 +142,5 @@ pub async fn game(ctx: &Context, interaction: &Interaction) -> Result<(), Error>
             inline: true,
         });
     }
-
-    ctx.interaction()
-        .update_response(&interaction.token)
-        .embeds(Some(&[embed.build()]))?
-        .await?;
-
-    Ok(())
+    embed.build()
 }
