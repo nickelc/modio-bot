79476faaec88486e2c35a840e884aa2124127a50
Constantin Nickel <constantin.nickel@gmail.com>
2023-01-24 05:43:28 +0100

    Fix migration file for dropping the prefix column

--- migrations/0008_drop_prefix/up.sql
+++ migrations/0008_drop_prefix/up.sql
@@ -1,1 +1,8 @@
-ALTER TABLE settings DROP COLUMN prefix;
+CREATE TABLE settings_new (
+    guild BIGINT PRIMARY KEY NOT NULL,
+    game INTEGER NULL
+);
+
+INSERT INTO settings_new (guild, game) SELECT guild, game FROM settings;
+DROP TABLE settings;
+ALTER TABLE settings_new RENAME TO settings;
